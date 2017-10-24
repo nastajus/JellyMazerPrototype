5a28640c9f31aeadf449fec558a546e5be9dae2f
Ian Nastajus <ian.nastajus@gmail.com>
2017-10-24 16:08:38 -0400

    pseudo message flow added in a big comment at top of GM_GameMgr

--- Assets/GM_GameMgr.cs
+++ Assets/GM_GameMgr.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GM_GameMgr : MonoBehaviour {
+// Purpose : this class manages overall executive state of the game, and initiates other classes / components / scenes / events
 
-	// Use this for initialization
-	void Start () {
+// pseudo message flow: 
+
+// 1. make itself a singleton
+// 2. initialize the event management system, all (?)  inter-class communication will (?) be via messaging.
+// 3. initialize SM_ScreenMgr, make it listen & wait.
+// 4. here use local enum to track various game states, set to "unitialized" state to begin.
+// 5. here fire off message to SM_ScreenMgr to enter state "get players"(), which in turn will cause loading both S1 scenes in tandem addititively. (also probably with EM)
+//      5b. consequently, above will cause PM_PlayerMgr to initialize, listen & wait.
+//      5c. PM_PlayerMgr will receive message to "get players"(), which will cause GPM_GamepadMgr to listen for controllers
+//      5d. GPM_GamepadMgr will look in an Update loop continuously for three essential tasks: 
+//              (i) detect any gamepads attached, and do somehow update the visual representation, probably directly messaging each "GamepadIcon" object.
+//              (ii) detect any button input at all from these gamepad, and use that to toggle the state of being an active player or not; probably direct message "PlayerIcon" object to spin and light up.
+//              (iii) for every "player confirm"(#) event triggered by any gamepad it will also fire a message off, intending for the GM_GameMgr to capture & react to.
+// 6. the GM_GameManager captures the "player confirm"(#) message from 5d.(iii)., and validates against it's own game logic if enough players are available to allow playing. if yes, changes game state enum to the following, and sends message to "getting players & can start game" (referred to as just ~"can start game"~ ).  similarly will be listening for unconfirms & do the reverse, reverting back game state enum to just "get players" if #s drop below game rules.
+// 7. the SM_ScreenManager captures the message ~"can start game"~ and visually enables (brightens) the "Start Game" button. if that button is clicked via mouse, it will send a message "new maze".
+// 8. the GM_GameManager captures the "make maze", which then advances it's local enum state accordingly, and then it sends a message to SM_ScreenMgr to somehow hide old canvas and (load an arbitrary timer canvas... don't actually care about this... it's a placeholder). So SM is listening for "make maze" in order to transition away from "getting players" UI canvas and instead enable ~playing-in-maze~ UI canvas in it's place.
+// 9. the MM_MazeMgr will run, it will make a really REALLY SIMPLE map (not even a maze), where it simply spawns user-controllable placeholder gameobjects, in a designated SpawnLocation (can all overlap for now). 
+//      9b. MM_MazeMgr will generate a simple layout of tiles as a temporary placeholder, a start location on the left, an end location on the right.
+//      9c. MM_MazeMgr will create spawn OnTriggerEnter region to detect end-level entry of players, and fire message "player exited"(#), which the GM_GameMgr will detect.
+// 10. GM_GameMgr will detect players have exited. Depending on game mode, it will have logic to handle accordingly. for now, let's just say any player that exits triggers "new map", and that this happens twice, and then credits roll.
+
+
+public class GM_GameMgr : MonoBehaviour
+{
+    public static GM_GameMgr Instance = null;
+    private EM_EventMgr EventMgr_;
+    private GPM_GamepadMgr GamepadMgr_;
+    private MM_MazeMgr MazeMgr_;
+    private SM_ScreenMgr ScreenMgr_;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
